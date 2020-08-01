12a6f92217c41972de1f00d9ec89fc9adb8cc51d
nnasoke <naruemit.nasoke@gmail.com>
2020-08-01 15:16:36 +0700

    remove @shared

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import App from "./App";
 import Env from "./Env";
-import logger from "@shared/logger";
+import logger from "./shared/logger";
 
 App.listen(Env.serverPort, Env.serverHost, () => {
   logger.info(`Server is running at ${Env.serverHost}:${Env.serverPort}.`);
